d2b41479edaa343c6eb0d08f7caf039d66fc4983
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2022-06-02 00:11:27 +0200

    new card dealer class + text color change

---
+++ app/src/main/java/cardsDealer/Dealer.java
@@ -0,0 +1,16 @@
+package cardsDealer;
+
+import cards.Card;
+
+public class Dealer {
+    /*
+
+     */
+    public static int calculateBestPlay(Card firstCard, Card secondCard)
+    {
+        int bestPlay = 0;
+
+
+        return bestPlay;
+    }
+}

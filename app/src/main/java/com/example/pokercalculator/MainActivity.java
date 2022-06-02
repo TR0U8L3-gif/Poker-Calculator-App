ad5488929d29809fe2b03058e544a2df0fd67f34
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2022-06-02 02:18:37 +0200

    Poker Calculator alpha version

--- app/src/main/java/com/example/pokercalculator/MainActivity.java
+++ app/src/main/java/com/example/pokercalculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import cards.Card;
@@ -21,6 +22,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     Spinner firstCardColor;
     Spinner secondCardSymbol;
     Spinner secondCardColor;
+
+    TextView foldEarly;
+    TextView callEarly;
+    TextView raiseEarly;
+
+    TextView foldMid;
+    TextView callMid;
+    TextView raiseMid;
+
+    TextView foldLate;
+    TextView callLate;
+    TextView raiseLate;
+
+    TextView foldBlind;
+    TextView callBlind;
+    TextView raiseBlind;
 
     Card firstCard = new Card(0,0);
     Card secondCard = new Card(0,0);
@@ -55,7 +72,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         secondCardSymbol.setOnItemSelectedListener(this);
         firstCardColor.setOnItemSelectedListener(this);
         secondCardColor.setOnItemSelectedListener(this);
+//        text area
+        foldEarly = (TextView) findViewById(R.id.foldEarly);
+        callEarly = (TextView) findViewById(R.id.callEarly);
+        raiseEarly = (TextView) findViewById(R.id.raiseEarly);
 
+        foldMid = (TextView) findViewById(R.id.foldMid);
+        callMid = (TextView) findViewById(R.id.callMid);
+        raiseMid = (TextView) findViewById(R.id.raiseMid);
+
+        foldLate = (TextView) findViewById(R.id.foldLate);
+        callLate = (TextView) findViewById(R.id.callLate);
+        raiseLate = (TextView) findViewById(R.id.raiseLate);
+
+        foldBlind = (TextView) findViewById(R.id.foldBlind);
+        callBlind = (TextView) findViewById(R.id.callBlind);
+        raiseBlind = (TextView) findViewById(R.id.raiseBlind);
     }
     private void changeCard(boolean card)
     {
@@ -68,7 +100,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             color = firstCard.getColor().toLowerCase();
             int resID = getResources().getIdentifier((color + symbol) , "drawable", getPackageName());
             firstCardImage.setImageResource(resID);
-//            Toast.makeText(getApplicationContext(),("FC: "+color+symbol),Toast.LENGTH_LONG).show();
         }
         else
         {
@@ -76,10 +107,200 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             color = secondCard.getColor().toLowerCase();
             int resID = getResources().getIdentifier((color + symbol) , "drawable", getPackageName());
             secondCardImage.setImageResource(resID);
-//            Toast.makeText(getApplicationContext(),("SC: "+color+symbol),Toast.LENGTH_LONG).show();
         }
-         String out = Integer.toString(Dealer.calculateBestPlay(firstCard,secondCard));
-        Toast.makeText(getApplicationContext(),out,Toast.LENGTH_LONG).show();
+        changeText(Dealer.calculateBestPlay(firstCard,secondCard));
+//        String out = Integer.toString(Dealer.calculateBestPlay(firstCard,secondCard));
+//        Toast.makeText(getApplicationContext(),out,Toast.LENGTH_LONG).show();
+    }
+
+    private void changeText(int option)
+    {
+        String call20 = "Call20";
+        String call = "Call";
+        String fold = "Fold";
+        String raise = "Raise";
+        String error = "Error";
+
+        if(option == 0)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(fold);
+
+            foldMid.setText(fold);
+            callMid.setText(fold);
+            raiseMid.setText(fold);
+
+            foldLate.setText(fold);
+            callLate.setText(fold);
+            raiseLate.setText(fold);
+
+            foldBlind.setText(fold);
+            callBlind.setText(fold);
+            raiseBlind.setText(fold);
+        }
+        else if(option == 1)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(fold);
+
+            foldMid.setText(fold);
+            callMid.setText(call);
+            raiseMid.setText(fold);
+
+            foldLate.setText(raise);
+            callLate.setText(call);
+            raiseLate.setText(fold);
+
+            foldBlind.setText(fold);
+            callBlind.setText(call);
+            raiseBlind.setText(fold);
+        }
+        else if(option == 2)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(fold);
+
+            foldMid.setText(fold);
+            callMid.setText(fold);
+            raiseMid.setText(fold);
+
+            foldLate.setText(raise);
+            callLate.setText(fold);
+            raiseLate.setText(fold);
+
+            foldBlind.setText(raise);
+            callBlind.setText(call);
+            raiseBlind.setText(fold);
+        }
+        else if(option == 3)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(fold);
+
+            foldMid.setText(fold);
+            callMid.setText(call);
+            raiseMid.setText(fold);
+
+            foldLate.setText(raise);
+            callLate.setText(call);
+            raiseLate.setText(fold);
+
+            foldBlind.setText(raise);
+            callBlind.setText(call);
+            raiseBlind.setText(fold);
+        }
+        else if(option == 4)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(fold);
+
+            foldMid.setText(fold);
+            callMid.setText(fold);
+            raiseMid.setText(fold);
+
+            foldLate.setText(raise);
+            callLate.setText(call);
+            raiseLate.setText(fold);
+
+            foldBlind.setText(raise);
+            callBlind.setText(call);
+            raiseBlind.setText(fold);
+        }
+        else if(option == 5)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(fold);
+
+            foldMid.setText(raise);
+            callMid.setText(fold);
+            raiseMid.setText(fold);
+
+            foldLate.setText(raise);
+            callLate.setText(raise);
+            raiseLate.setText(fold);
+
+            foldBlind.setText(raise);
+            callBlind.setText(call);
+            raiseBlind.setText(fold);
+        }
+        else if(option == 6 || option == 9 )
+        {
+            foldEarly.setText(raise);
+            callEarly.setText(raise);
+            raiseEarly.setText(raise);
+
+            foldMid.setText(raise);
+            callMid.setText(raise);
+            raiseMid.setText(raise);
+
+            foldLate.setText(raise);
+            callLate.setText(raise);
+            raiseLate.setText(raise);
+
+            foldBlind.setText(raise);
+            callBlind.setText(raise);
+            raiseBlind.setText(raise);
+        }
+        else if(option == 7)
+        {
+            foldEarly.setText(fold);
+            callEarly.setText(fold);
+            raiseEarly.setText(call20);
+
+            foldMid.setText(call);
+            callMid.setText(call);
+            raiseMid.setText(call20);
+
+            foldLate.setText(raise);
+            callLate.setText(call);
+            raiseLate.setText(call20);
+
+            foldBlind.setText(call);
+            callBlind.setText(call);
+            raiseBlind.setText(call20);
+        }
+        else if(option == 8 )
+        {
+            foldEarly.setText(raise);
+            callEarly.setText(raise);
+            raiseEarly.setText(call20);
+
+            foldMid.setText(raise);
+            callMid.setText(raise);
+            raiseMid.setText(call20);
+
+            foldLate.setText(raise);
+            callLate.setText(raise);
+            raiseLate.setText(call20);
+
+            foldBlind.setText(raise);
+            callBlind.setText(raise);
+            raiseBlind.setText(call20);
+        }
+        else
+        {
+            foldEarly.setText(error);
+            callEarly.setText(error);
+            raiseEarly.setText(error);
+
+            foldMid.setText(error);
+            callMid.setText(error);
+            raiseMid.setText(error);
+
+            foldLate.setText(error);
+            callLate.setText(error);
+            raiseLate.setText(error);
+
+            foldBlind.setText(error);
+            callBlind.setText(error);
+            raiseBlind.setText(error);
+        }
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -106,12 +327,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             secondCard.setColor(position);
             changeCard(true);
         }
-
-//        Toast.makeText(getApplicationContext(),out,Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
+        Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_SHORT).show();
     }
 }
